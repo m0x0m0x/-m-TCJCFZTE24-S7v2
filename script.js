60b686ac673e267eb38f2c9a46870aeb0985951d
m0x0m0x <mx0mx@tuta.io>
2024-02-23 15:15:33 +0530

    add button function

--- script.js
+++ script.js
@@ -16,4 +16,15 @@
 // console.log(document.querySelector(".guess").value);
 
 // Setting the function on check button L20 index.html
-document.querySelector(".check").addEventListener("click");
+document.querySelector(".check").addEventListener("click", function () {
+  // This statemetn will grab the value which inside the guess class
+  const guess = document.querySelector(".guess").value;
+  console.log("Guess Value", guess);
+
+  console.log(`
+  How many suck like her ass ? :- 
+  ${document.querySelector(".guess").value}
+  `);
+
+  //   document.querySelector(".message").textContent = "SuckandFuck!";
+});
