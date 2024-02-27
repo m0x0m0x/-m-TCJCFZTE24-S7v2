de1e280b322af27c25429b3f0cdf0c6fecce29a4
m0x0m0x <mx0mx@tuta.io>
2024-02-27 14:52:14 +0530

    Add finals

--- script.js
+++ script.js
@@ -5,18 +5,24 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  console.log("ClosedFucked");
+};
+
+const openModal = function () {
+  console.log("ButtonRaped");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 // Selecting modals with the same name
 const btnsShowModal = document.querySelectorAll(".show-modal");
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener("click", function () {
-    console.log("ButtonRaped");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  btnsShowModal[i].addEventListener("click", openModal);
   btnsShowModal[i].textContent = "👃Sniff";
-
-  btnCloseModal.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
 }
+// Function for hiding the modals
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
